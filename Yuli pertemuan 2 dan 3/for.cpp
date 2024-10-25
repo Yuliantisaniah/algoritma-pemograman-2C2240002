3e981b5d4bb08a7b1d0186236ebd61ff3a4fd0a0
Yuliantisaniah <yuliantisaniah23@gmail.com>
2024-10-25 16:36:15 +0700

    Add files via upload

---
+++ Yuli pertemuan 2 dan 3/for.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int nilai_awal, nilai_akhir;
+    
+    // Meminta input dari user untuk nilai a dan b
+    cout << "Masukkan nilai a: ";
+    cin >> nilai_awal;
+    cout << "Masukkan nilai b: ";
+    cin >> nilai_akhir;
+
+    // Perulangan dari a hingga b
+    for (int i = nilai_awal; i <= nilai_akhir; i++) {
+        cout << i << ".classyscience.ui" << endl;  // Menampilkan nilai i
+    }
+
+    return 0;
+}
+
